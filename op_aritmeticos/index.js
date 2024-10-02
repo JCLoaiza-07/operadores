78035a1cea52570a2b6036a9590341f797e2e1bc
JCLoaiza-07 <juan.loaiza089@asys.edu.co>
2024-10-01 19:31:26 -0500

    He subido a mi repositorio los operadores aritméticos

---
+++ op_aritmeticos/index.js
@@ -0,0 +1,29 @@
+//Suma
+let resultadoSuma = 10 + 5;
+console.log(resultadoSuma); //Output: 15
+
+//Resta
+let resultadoResta = 20 - 7;
+console.log(resultadoResta); //Output: 13
+
+//Multiplicación
+let resultadoMultiplicacion = 3 * 4;
+console.log(resultadoMultiplicacion); //Output: 12
+
+//División
+let resultadoDivision = 16 / 4;
+console.log(resultadoDivision); //Output: 4
+
+//Residuo
+let resultadoResiduo = 10 % 3;
+console.log(resultadoResiduo); //Output: 1
+
+//Incremento
+let numero = 1;
+numero++;
+console.log(numero); //Output: 2
+
+//Decremento
+let otroNumero = 5;
+otroNumero--;
+console.log(otroNumero); //Output: 4
